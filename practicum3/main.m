d610cad4958d0bef232d1507cca264e55b3f18bd
lab <lab@wm320-a229579i.campus.ithaca.lan>
2014-02-27 09:47:25 -0500

    Initial Commit

---
+++ practicum3/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  practicum3
+//
+//  Created by lab on 2/27/14.
+//  Copyright (c) 2014 Computer Science. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
